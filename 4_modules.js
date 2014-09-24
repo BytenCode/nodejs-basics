a5db27617af8db1fcf04129585a54215db02da98
Filipe Lopes <filipe@bytencode.com>
2014-09-25 00:00:43 +0100

    Node modules - custom modules

--- 4_modules.js
+++ 4_modules.js
@@ -10,7 +10,5 @@ var num1 = parseInt(process.argv[2]);
 var operator = process.argv[3];
 var num2 = parseInt(process.argv[4]);
 
-//console.log(mymodule);
-//mymodule.calculate();
 var result = mymodule.calculate(num1, operator, num2);
 mymodule.print(num1, operator, num2, result);

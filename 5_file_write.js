ccd81671f17e69cec2539c4e0f75b6831fc27ea3
Filipe Lopes <filipe@bytencode.com>
2014-09-25 22:03:54 +0100

    Node Write Files - Create a simple example of make logs

--- 5_file_write.js
+++ 5_file_write.js
@@ -2,12 +2,14 @@ var fs = require('fs');
 fs.writeFileSync('5_temp_file_sync.txt', 'Byte&Code Rules!!');
 console.log('The \'File 5_temp_file_sync.txt\' is saved!');
 
-fs.writeFile('5_temp_file_async.txt', 'Hello Byte&Code!', function (error) {
+var message = 'Hello Byte&Code!';
+fs.writeFile('5_temp_file_async.txt', message, function (error) {
 	if (error) throw error;
 	console.log('The \'File 5_temp_file_async.txt\' is saved!');
 });
 
-fs.appendFile('5_temp_append_file.txt', '\nHello Byte&Code!', function (err) {
+message = new Date().toISOString() + ' > ' + message;
+fs.appendFile('5_temp_append_file.txt', '\n' + message, function (err) {
 	if (err) throw err;
-	console.log('The \'Hello Byte&Code!\' was appended to file 5_temp_append_file.txt!');
+	console.log('The \'' + message + '\' was appended to file 5_temp_append_file.txt!');
 });

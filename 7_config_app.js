46f66b511a5deb7b7c825b4f78432892f3ce76ad
Filipe Lopes <filipe@bytencode.com>
2014-09-25 22:14:54 +0100

    Node Create a simple configuration file

---
+++ 7_config_app.js
@@ -0,0 +1,4 @@
+var appConfig = require('./7_config_file');
+
+console.log("config.host: " + appConfig.host);
+console.log("config.port: " + appConfig.port);
---
+++ 7_config_file.js
@@ -0,0 +1,6 @@
+var config = {
+	host : 'www.bytencode.com',
+	port : 80
+}
+
+module.exports = config;
